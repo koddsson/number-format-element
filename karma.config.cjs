8a9bbbdaeb9fa63b43b378bec8e52f498ce8b05c
Kristján Oddsson <koddsson@github.com>
2022-04-29 10:37:29 +0100

    Initial commit

---
+++ karma.config.cjs
@@ -0,0 +1,19 @@
+process.env.CHROME_BIN = require('chromium').path
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: [
+      {pattern: 'dist/custom-element.js', type: 'module'},
+      {pattern: 'test/test.js', type: 'module'}
+    ],
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    singleRun: true,
+    concurrency: Infinity
+  })
+}
